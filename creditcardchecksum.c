40b9e7ebe57fed1e8c023c92804beeffe7dbc22c
James Goldstein <jamesdylangoldstein@gmail.com>
2016-06-07 18:58:53 -0700

    Update creditcardchecksum.c

--- creditcardchecksum.c
+++ creditcardchecksum.c
@@ -17,14 +17,15 @@ unsigned long long creditcardlength, creditcardnum, base, exponent;
 
 
 int main(int argc, const char * argv[]) {
-    printf("Type a long number: ");
+    printf("Type a credit card number: ");
     creditcardnum = GetLongLong();
     creditcardlength = NumDigits(creditcardnum);
     unsigned long long digitsarray[creditcardlength];
     
     unsigned long long creditcardmanip = creditcardnum;
-    unsigned long long countera, counterc, counterd, firstsumdigits, timesninemodzero, finalsum;
-    unsigned long long counterb = creditcardlength - 1;
+    long long countera, counterd, firstsumdigits, timesninemodzero, finalsum;
+    long long counterb = creditcardlength - 1;
+    long long counterc = creditcardlength - 1;
 
     //Assigns each digit of the credit card number to a place in an array
     for(countera = 0; countera < creditcardlength; countera++)
@@ -37,27 +38,52 @@ int main(int argc, const char * argv[]) {
     //Identify the type of credit card
     if((digitsarray[0] == 3 && digitsarray[1] == 4) || (digitsarray[0] == 3 && digitsarray[1] == 7))
     {
-        printf("This is an American Express card.\n");
+        if(creditcardlength == 15)
+        {
+            printf("This is an American Express card.\n");
+        }
+        else
+        {
+            printf("Not a known credit card brand.\n");
+        }
     }
     else if(digitsarray[0] == 4)
     {
-        printf("This is a Visa card.\n");
+        if(creditcardlength == 13 || creditcardlength == 16)
+        {
+            printf("This is a Visa card.\n");
+        }
+        else
+        {
+            printf("Not a known credit card brand.\n");
+        }
     }
-    else if(digitsarray[0] == 5 && digitsarray[1] == 1 || digitsarray[1] == 2 || digitsarray[1] == 3 || digitsarray[1] == 4 || digitsarray[1] == 5)
+    else if((digitsarray[0] == 5 && digitsarray[1] == 1) || (digitsarray[1] == 2 || digitsarray[1] == 3) || (digitsarray[1] == 4) || (digitsarray[1] == 5))
     {
-        printf("This is a MasterCard.\n");
+        if(creditcardlength == 16)
+        {
+            printf("This is a MasterCard.\n");
+        }
+        else
+        {
+            printf("Not a known credit card brand.\n");
+        }
     }
     
     //Starting with the right-most digit, double that digit and do that to every other digit
     //Then if the new doubled number is two digits (example: 7 * 2 = 14)
     //Make the new doubled number one digit by adding both digits together (7 * 2 = 1 + 4)
-    for(counterc = creditcardlength - 1; counterc < 0; counterc - 2)
-    {
-        digitsarray[counterc] = digitsarray[counterc]*2;
-        if(digitsarray[counterc] > 9)
-        {
-            digitsarray[counterc] = digitsarray[counterc] % 10 + 1;
-        }
+    //counterc is -1 because a 16 digit card becomes 0-15
+    printf("counterc = %llu\n", counterc);
+    
+   while(counterc >= 0)
+   {
+       digitsarray[counterc] *= 2;
+       if(digitsarray[counterc] > 9)
+       {
+           digitsarray[counterc] = digitsarray[counterc] % 10 + 1;
+       }
+       counterc -= 2;
     }
     
     //Sum all the new doubled digits together and all of the remaining original digits
